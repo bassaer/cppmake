b19f1e8e478876cc1517436d9895ac5f966d241a
Tsubasa Nakayama <app.nakayama@gmail.com>
2017-11-18 18:43:34 +0900

    add file creator func

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
+
+void makeDir(string dir)
+{
+  string command = "mkdir " + dir;
+  system(command.c_str());
+}
+
+void createFile(string inPath, string outPath)
+{
+  ifstream inFile(inPath.c_str());
+  ofstream outFile(outPath.c_str());
+  string line;
+  while(getline(inFile, line))
+  {
+    outFile << line << endl;
+  }
+  inFile.close();
+  outFile.close();
+}
+
 int main(int argc, char* argv[]) 
 {
   if (argc != 2)
   {
-    cout << "Error" << endl;
+    cout << "Error please type project name." << endl;
     exit(1);
   }
-  string command = argv[1];
-  //command += " &";
-  system(command.c_str());
+  string project = argv[1];
+  makeDir("-p " + project + "/{src,tests,bin,obj,include}");
+  createFile("/usr/local/cppmake/Makefile", project + "/Makefile");
   return 0;
 }
 
